acb47dfe3ce3a9dc1e8b128f73b0422b5e10dd26
Punkbooster <2morrow2late@rambler.ru>
2015-09-28 20:30:32 +0200

    Search function

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,14 @@ class Book < ActiveRecord::Base
 	scope :finished, ->{ where.not(finished_on: nil) } #checks if book is not finished
 	scope :recent, ->{ where('finished_on > ?', 2.days.ago) } #recent scope
 
+	def self.search(keyword)
+		if keyword.present?
+			where(title: keyword)
+		else
+			all
+		end
+	end
+
 	def finished?
 		finished_on.present?
 	end
