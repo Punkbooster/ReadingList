cb23f2d4dc2d1b5ba39faa71fcd2f430f057d8c9
Punkbooster <2morrow2late@rambler.ru>
2015-09-28 20:55:54 +0200

    Improve search

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,9 +3,11 @@ class Book < ActiveRecord::Base
 	scope :finished, ->{ where.not(finished_on: nil) } #checks if book is not finished
 	scope :recent, ->{ where('finished_on > ?', 2.days.ago) } #recent scope
 
+	before_save :set_keywords
+
 	def self.search(keyword)
 		if keyword.present?
-			where(title: keyword)
+			where('keywords LIKE ?', "%#{keyword.downcase}%")
 		else
 			all
 		end
@@ -13,6 +15,12 @@ class Book < ActiveRecord::Base
 
 	def finished?
 		finished_on.present?
+	end
+
+	protected
+
+	def set_keywords
+		self.keywords = [title, author, description].map { |p| p.downcase }.join(' ') #join our array with spaces
 	end
 
 end
