dd355e190d6c2e075c79e5180b69907166737755
Punkbooster <2morrow2late@rambler.ru>
2015-09-28 18:31:53 +0200

    Create recent & finished scopes

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,8 @@
 class Book < ActiveRecord::Base
 
+	scope :finished, ->{ where.not(finished_on: nil) } #checks if book is not finished
+	scope :recent, ->{ where('finished_on > ?', 2.days.ago) } #recent scope
+
 	def finished?
 		finished_on.present?
 	end
